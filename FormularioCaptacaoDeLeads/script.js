8a0bfbb438e7c04424c1564f81a9c0bc343b512d
Nathalia Mariano Lopes <101136711+nnathalia@users.noreply.github.com>
2024-02-16 09:37:51 -0400

    update

--- FormularioCaptacaoDeLeads/script.js
+++ FormularioCaptacaoDeLeads/script.js
@@ -1,8 +1,8 @@
 
- function check(event) {
-    const checkbox = event.target;
-    const card = checkbox.closest('.card-img');
-    card.classList.toggle('card-check');
+function check(event) {
+  const checkbox = event.target;
+  const card = checkbox.closest('.card-img');
+  card.classList.toggle('card-check');
 }
 
 const checkboxes = document.querySelectorAll('.checkbox');
@@ -10,4 +10,4 @@ const checkboxes = document.querySelectorAll('.checkbox');
 for (const checkbox of checkboxes) {
   checkbox.addEventListener('click', check);
 }
- 
+
