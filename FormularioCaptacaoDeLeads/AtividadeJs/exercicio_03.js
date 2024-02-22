56b805dc9ed417f409aa5e846d766b4fb3c26bf8
Nathalia Mariano Lopes <101136711+nnathalia@users.noreply.github.com>
2024-02-21 22:14:04 -0400

    Add Atividade Js

---
+++ FormularioCaptacaoDeLeads/AtividadeJs/exercicio_03.js
@@ -0,0 +1,11 @@
+/*Faça um algoritmo que receba a matrícula e duas notas do aluno. 
+Calcular a média e mostrar a matrícula do aluno com as seguintes mensagens de acordo com os dados a seguir:
+Fazer um vetor de objeto de aluno. O objeto aluno deve ter nome, nota1, noata2 e nota3. 
+Ao final mostre a média geral dos alunos. */
+
+let aluno = [
+    nome = "Thor",
+    nota1 = 8.0,
+    nota2 = 9.5
+]
+
