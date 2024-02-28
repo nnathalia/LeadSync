5e2cb9fbea4996a4353926e312d4f7814c3e9670
Nathalia Mariano Lopes <101136711+nnathalia@users.noreply.github.com>
2024-02-27 23:17:39 -0400

    update

--- FormularioCaptacaoDeLeads/AtividadeJs/exercicio_03.js
+++ FormularioCaptacaoDeLeads/AtividadeJs/exercicio_03.js
@@ -3,9 +3,26 @@ Calcular a média e mostrar a matrícula do aluno com as seguintes mensagens de 
 Fazer um vetor de objeto de aluno. O objeto aluno deve ter nome, nota1, noata2 e nota3. 
 Ao final mostre a média geral dos alunos. */
 
-let aluno = [
-    nome = "Thor",
-    nota1 = 8.0,
-    nota2 = 9.5
-]
+let aluno = {
+    nome : "Thor",
+    matricula : 10102030,
+    nota1 : 80,
+    nota2 : 95,
+    nota3 : 70
+}
+
+let media = (aluno.nota1 + aluno.nota2 + aluno.nota3) / 3
+
+console.log(`O aluno ${aluno.nome}
+Número de matricula: ${aluno.matricula}`)
+
+if(media > 70){
+    console.log(`Foi aprovado!`)
+}
+else if(media == 70){
+    console.log(`Está em recuperação!`)
+}
+else{
+    console.log(`Foi reprovado!`)
+}
 
