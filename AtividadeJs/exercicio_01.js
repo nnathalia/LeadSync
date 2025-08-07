6f992123455feaea487ef6ea29433b46dfcaf4bc
nathalia <nmarianolopes@gmail.com>
2025-08-07 00:30:33 -0400

    update projeto 🚀

--- AtividadeJs/exercicio_01.js
+++
@@ -1,9 +0,0 @@
-// 1.Criar 4 variáveis para receber as notas de um aluno, calcular e mostrar a média desse aluno.
-
-let aluno = "Steve Rogers"
-let nota1 = 8.0
-let nota2 = 7.5
-
-let media = (nota1+ nota2 )/2
-
-console.log(`A média do aluno ${aluno} é ${media}`)
